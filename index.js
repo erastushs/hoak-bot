3c1c99b5ef75a90162e650b4c72cc6a72a2731a5
Erastus-HS <erastushs@gmail.com>
2021-12-01 03:26:56 +0900

    mengganti pesan menjadi embed dan menambahkan pesan help

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-const Discord = require('discord.js')
-const bot = new Discord.Client
+const {
+    Client, 
+    MessageEmbed
+} = require('discord.js')
+const bot = new Client
 
 const token = 'OTE1MjY4MTM5MzE3NjA4NTA5.YaZH4Q.uH401K6qKG6kEOOpwTGeRYP30Ts'
 var PREFIX = 'hoak'
@@ -18,17 +21,19 @@ bot.on('message', message => {
             break
 
 
-        case 'info' :
-            if(!args[1]) {
-                message.channel.send('Commands info) ; \n-author\n-version')
-            }
-            if (args[1] === 'author'){
-                message.channel.send('This bot was created by <@398542246523437066>')
-                break
-            } else if (args[1] === 'version'){
-                message.channel.send('Version : ' + version)
-                break
-            } 
+        case 'info':
+            const info = new MessageEmbed()
+                .setTitle(' Hoak Fam Information')
+                .setDescription('**Author** : <@398542246523437066>\n **Version** : ' + version)
+            message.channel.send(info)
+        break
+            
+        case 'help':
+        const help = new MessageEmbed()
+                .setTitle(' Hoak Fam Help Menu')
+                .addField('My prefix is `hoak`. Use `hoak<command>` to call me ', '[hoakinfo](https://s.id/HoakFam) : untuk informasi bot\n[hoakinfo](https://s.id/HoakFam) : untuk informasi bot')
+            message.channel.send(help)
+        break
     }
 })
 
