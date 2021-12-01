631334b37c728ee57ca17da2ac9bbee75785eecb
Erastus-HS <erastushs@gmail.com>
2021-12-01 13:43:03 +0900

    menambahkan fitur cooldown

--- commands/info.js
+++ commands/info.js
@@ -1,11 +1,17 @@
 const {
-    MessageEmbed
+    MessageEmbed,
+    Collection
 } = require ('discord.js')
+const cooldown = new Collection()
 const botInfo = require('./bot-info.json')
+
 module.exports = {
     name: 'info',
     description: 'Kolom informasi',
     execute(Message) {
+        if (cooldown.has(Message.author.id)){
+            Message.reply('Tolong jangan spam. Tunggu 10 detik')
+        } else {
         const info = new MessageEmbed()
         .setTitle(' Hoak Fam Information')
         .addField('Author',  botInfo.creator)
@@ -13,5 +19,10 @@ module.exports = {
         .addField('Library', botInfo.library)
         .setColor('#34ebe8')
     Message.channel.send(info)
+    cooldown.set(Message.author.id)
+    setTimeout(() => {
+        cooldown.delete(Message.author.id)
+    }, 1000 * 10);
+        }
     }
 }
