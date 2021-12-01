402297538fddf8c55fce41fbbe1f3a48e5bd5a73
Erastus-HS <erastushs@gmail.com>
2021-12-02 07:31:03 +0900

    editing testAdmin

--- commands/testAdmin.js
+++ commands/testAdmin.js
@@ -2,7 +2,8 @@ module.exports = {
     name: 'testAdmin',
     description: 'Testing admin',
     execute(Message) {
-        var role= Message.member.roles.cache.find(r => r.name ==='🔱Assistant of Hoak🔱', '⚜️Father of Hoak⚜️')
+        var role= Message.member.roles.cache.find(r => r.name ==='🔱Assistant of Hoak🔱')
+        var role= Message.member.roles.cache.find(r => r.name ==='⚜️Father of Hoak⚜️')
         if (role) {
             Message.channel.send('Halo Admin')
         } else {
