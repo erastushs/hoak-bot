19a8b7dc1274704a50c69958c51b37d58990f733
Erastus Hendro Setiono <70123969+erastushs@users.noreply.github.com>
2022-08-29 22:19:53 +0900

    Menambahkan roles yg bisa akses clear

--- commands/commands/clear.js
+++ commands/commands/clear.js
@@ -3,7 +3,7 @@ module.exports = {
   description: "delete messages in text channel",
   execute(msg, args) {
     var role = msg.member.roles.cache.some((r) =>
-      ["🔱Assistant of Hoak🔱", "⚜️Father of Hoak⚜️"].includes(r.name)
+      ["🔱Assistant of Hoak🔱", "⚜️Father of Hoak⚜️", "Assistant of Hoak","Father of Hoak"].includes(r.name)
     );
     if (role) {
       if (!args[1]) {
